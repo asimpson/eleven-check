c9f25b91b607791cf358de96254097a7c79fb48d
Adam Simpson <adam@heysparkbox.com>
2017-02-11 17:46:14 -0500

    :sparkles: add better output

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const path = require('path');
 const eventParse = require('./lib/eventParse');
 const check = require(path.resolve(process.env.PWD, process.argv[2]));
 const event = process.argv[3] ? eventParse(process.argv[3]) : {};
+const Table = require('cli-table');
+const emoji = require('node-emoji');
 
 const callback = (err, data) => {
+  const table = new Table({
+    head: [],
+  });
+
   if (err) {
-    console.log('++++');
-    console.log('err', err);
-    console.log('++++');
+    table.push(
+      [path.resolve(process.env.PWD, process.argv[2])],
+      [`${emoji.get('fire')} ${err}`]
+    );
   } else {
-    console.log('====');
-    console.log('data', data);
-    console.log('====');
+    table.push(
+      [path.resolve(process.env.PWD, process.argv[2])],
+      [`${emoji.get('white_check_mark')} ${data}`]
+    );
   }
+
+  console.log(table.toString());
 };
 
 check.handler(event, {}, callback);
